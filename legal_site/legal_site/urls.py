218ec16be9c4c45a4c584054496b9265f1f25799
s24675 <s24675@pjwstk.edu.pl>
2025-08-20 22:37:03 +0200

    admin url changed

--- legal_site/legal_site/urls.py
+++ legal_site/legal_site/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
 ]
 
 urlpatterns += i18n_patterns(
-    path('admin/', admin.site.urls),
+    path('fixCallAdm/', admin.site.urls),
 
     path('', include('main.urls')),  # ТОЛЬКО здесь допустимо использовать внутри i18n_patterns
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
