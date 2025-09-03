4fcb16b14c81f7b61fc7c1914d13009930566b43
s24675 <s24675@pjwstk.edu.pl>
2025-09-03 16:42:57 +0200

    Urls fix

--- legal_site/main/urls.py
+++ legal_site/main/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     #path('', views.maintenance, name='maintenance'),
     path('', views.index, name='index'),
-    path('working', views.index, name='index'),
+    #path('working', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('policy/',views.privacy_policy, name='policy'),
